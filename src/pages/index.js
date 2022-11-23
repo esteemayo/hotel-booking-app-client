6f014acd22ea44d47634c1fb3cc6e70049d4526e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 01:19:05 +0100

    feat: import and export hotel and shared layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Home from './home/Home';
+import Hotel from './hotel/Hotel';
 import Layout from './layout/Layout';
 import List from './list/List';
+import SharedLayout from './layout/SharedLayout';
 
-export { Home, Layout, List };
+export { Home, Hotel, Layout, List, SharedLayout };
