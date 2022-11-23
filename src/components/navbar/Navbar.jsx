2af50e4a77516d4988cabdd16579f250aa83a705
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 02:22:56 +0100

    feat: add navbar contents

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,15 @@ import './navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='navbar__container'>
+        <span className='navbar__logo'>Bookingapp</span>
+        <div className='navbar__items'>
+          <button className='navbar__items--button'>Register</button>
+          <button className='navbar__items--button'>Login</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
