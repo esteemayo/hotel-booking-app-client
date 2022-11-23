fb3b89d75e6c0e2f84bfde39cdb648989bcf9e8a
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 02:27:16 +0100

    feat: add header

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
+import Header from 'components/header/Header';
 import Navbar from 'components/navbar/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <Navbar />
+      {pathname === '/' && <Header />}
       <ToastContainer />
       <Outlet />
     </>
