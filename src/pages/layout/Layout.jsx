9e158291ac7b3a01a4135dc17828de7b04729f9b
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 01:50:59 +0100

    feat: add navbar component

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
+import Navbar from 'components/navbar/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
   return (
     <>
+      <Navbar />
       <ToastContainer />
       <Outlet />
     </>
