f1d87bb9c634f6fee6120f806db6cd16bc27177a
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 01:25:59 +0100

    feat: import notFound page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Home from './home/Home';
 import Hotel from './hotel/Hotel';
 import Layout from './layout/Layout';
 import List from './list/List';
+import NotFound from './notFound/NotFound';
 import SharedLayout from './sharedLayout/SharedLayout';
 
-export { Home, Hotel, Layout, List, SharedLayout };
+export { Home, Hotel, Layout, List, NotFound, SharedLayout };
