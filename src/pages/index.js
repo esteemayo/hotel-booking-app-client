fc700f97f9ca816349c0da60d72f8751dea77165
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-23 01:23:46 +0100

    feat: change path

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Home from './home/Home';
 import Hotel from './hotel/Hotel';
 import Layout from './layout/Layout';
 import List from './list/List';
-import SharedLayout from './layout/SharedLayout';
+import SharedLayout from './sharedLayout/SharedLayout';
 
 export { Home, Hotel, Layout, List, SharedLayout };
