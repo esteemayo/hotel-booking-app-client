5a83ddbb0ab27484c7d4d585544378b4ca8f83e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-25 00:43:51 +0100

    modified

--- src/components/propertyList/ProperList.jsx
+++ src/components/propertyList/ProperList.jsx
@@ -14,8 +14,52 @@ const ProperList = () => {
           <h2>233 hotels</h2>
         </div>
       </div>
+      <div className='propertyList__item'>
+        <img
+          src='https://t-cf.bstatic.com/xdata/images/city/square250/661041.webp?k=9b623dbbbf3f001248b38d9e75f2a71f985b44436b2fd871d2e6153c3d76a9be&o='
+          alt=''
+          className='propertyList__img'
+        />
+        <div className='propertyList__title'>
+          <h1>Apartment</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className='propertyList__item'>
+        <img
+          src='https://t-cf.bstatic.com/xdata/images/city/square250/955874.webp?k=4a962e9b93f968482ab2a4df07a766be2ba265fb473abbc02c855560a33f4425&o='
+          alt=''
+          className='propertyList__img'
+        />
+        <div className='propertyList__title'>
+          <h1>Resorts</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className='propertyList__item'>
+        <img
+          src='https://t-cf.bstatic.com/xdata/images/city/square250/853006.webp?k=49f75610fb56878eb04a87432a9fa76f3b3e3d68383996d46c7dbbf53c5467c2&o='
+          alt=''
+          className='propertyList__img'
+        />
+        <div className='propertyList__title'>
+          <h1>Villas</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
+      <div className='propertyList__item'>
+        <img
+          src='https://t-cf.bstatic.com/xdata/images/city/square250/815261.webp?k=96c6465292cad5f9afffb2925a9f76b126d4675423300a6e63917f1fcf459a19&o='
+          alt=''
+          className='propertyList__img'
+        />
+        <div className='propertyList__title'>
+          <h1>Cabins</h1>
+          <h2>2331 hotels</h2>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProperList
+export default ProperList;
