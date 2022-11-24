85bd29332f39f2717007fa6530f6cfa845e8f553
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-24 02:55:39 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, Hotel, Layout, List, NotFound, SharedLayout } from 'pages';
+import { Home, Hotel, Layout, List, NotFound } from 'pages';
 
 function App() {
   return (
