fe94c0956322aab9f8f8f0db979dc52a47ee314f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-24 18:35:26 +0100

    created new component

---
+++ src/components/propertyList/ProperList.jsx
@@ -0,0 +1,21 @@
+import './propertyList.scss';
+
+const ProperList = () => {
+  return (
+    <div className='propertyList'>
+      <div className='propertyList__item'>
+        <img
+          src='https://t-cf.bstatic.com/xdata/images/city/square250/808081.webp?k=741722d4e7757b648faf551d5566d2259d3a0f31824f5893aab9f37f43fc7232&o='
+          alt=''
+          className='propertyList__img'
+        />
+        <div className='propertyList__title'>
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProperList
