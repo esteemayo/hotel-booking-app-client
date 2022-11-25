1f5c6848f9f616dacfdfb280f69678e410bf71f3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-25 01:36:27 +0100

    feat: add FeaturedProperties component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from 'components/featured/Featured';
+import FeaturedProperties from 'components/featuredProperties/FeaturedProperties';
 import ProperList from 'components/propertyList/ProperList';
 import './home.scss';
 
@@ -8,6 +9,8 @@ const Home = () => {
       <Featured />
       <h1 className='home__title'>Browse by property type</h1>
       <ProperList />
+      <h1 className='home__title'>Home guests love</h1>
+      <FeaturedProperties />
     </div>
   );
 };
