6071efc62cf91172cdb9097ad4ff0be8e27333ab
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-25 01:38:43 +0100

    feat: add MailList component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Featured from 'components/featured/Featured';
 import FeaturedProperties from 'components/featuredProperties/FeaturedProperties';
+import MailList from 'components/mailList/MailList';
 import ProperList from 'components/propertyList/ProperList';
 import './home.scss';
 
@@ -11,6 +12,7 @@ const Home = () => {
       <ProperList />
       <h1 className='home__title'>Home guests love</h1>
       <FeaturedProperties />
+      <MailList />
     </div>
   );
 };
