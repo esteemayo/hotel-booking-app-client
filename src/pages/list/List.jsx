478e1c718d560f3ca55834db579b1df3c4fc4afa
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-26 00:36:51 +0100

    feat: modify

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { format } from 'date-fns';
+import { DateRange } from 'react-date-range';
 import { useLocation } from 'react-router-dom';
 
 import Header from 'components/header/Header';
@@ -8,6 +9,7 @@ import './list.scss';
 const List = () => {
   const { state } = useLocation();
   const [date, setDate] = useState(state.date);
+  const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(state.options);
   const [destination, setDestination] = useState(state.destination);
 
@@ -23,10 +25,57 @@ const List = () => {
               <input type='text' id='destination' placeholder={destination} />
             </div>
             <div className='list__search--item'>
-              <label htmlFor='date'>Check-in-date</label>
-              <span>
+              <label>Check-in-date</label>
+              <span onClick={() => setOpenDate(!openDate)} className='searchText'>
                 {`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(date[0].endDate, 'MM/dd/yyyy')}`}
               </span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                  className='date'
+                />
+              )}
+            </div>
+            <div className='list__search--item'>
+              <label>Options</label>
+              <div className='option__item'>
+                <span className='option__text'>
+                  Min price <small>per night</small>
+                </span>
+                <input type='number' className='option__input' />
+              </div>
+              <div className='option__item'>
+                <span className='option__text'>
+                  Max price <small>per night</small>
+                </span>
+                <input type='number' className='option__input' />
+              </div>
+              <div className='option__item'>
+                <span className='option__text'>Adult</span>
+                <input
+                  type='number'
+                  className='option__input'
+                  placeholder={options.adults}
+                />
+              </div>
+              <div className='option__item'>
+                <span className='option__text'>Children</span>
+                <input
+                  type='number'
+                  className='option__input'
+                  placeholder={options.children}
+                />
+              </div>
+              <div className='option__item'>
+                <span className='option__text'>Room</span>
+                <input
+                  type='number'
+                  className='option__input'
+                  placeholder={options.room}
+                />
+              </div>
             </div>
           </div>
           <div className='list__result'></div>
