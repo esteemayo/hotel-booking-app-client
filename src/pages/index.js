c1a2581a68fd9053d01a5aa88cf6e435067cdf9a
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-11-27 12:19:07 +0100

    feat: remove shared layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,5 @@ import Hotel from './hotel/Hotel';
 import Layout from './layout/Layout';
 import List from './list/List';
 import NotFound from './notFound/NotFound';
-import SharedLayout from './sharedLayout/SharedLayout';
 
-export { Home, Hotel, Layout, List, NotFound, SharedLayout };
+export { Home, Hotel, Layout, List, NotFound };
