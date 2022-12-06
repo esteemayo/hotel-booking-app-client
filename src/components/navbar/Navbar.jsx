6e46616830975ce05e29e12f815436b8defac382
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-06 21:05:55 +0100

    feat: add link to app logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './navbar.scss';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar__container'>
-        <span className='navbar__logo'>Bookingapp</span>
+        <span className='navbar__logo'>
+          <Link to='/'>Bookingapp</Link>
+        </span>
         <div className='navbar__items'>
           <button className='navbar__items--button'>Register</button>
           <button className='navbar__items--button'>Login</button>
