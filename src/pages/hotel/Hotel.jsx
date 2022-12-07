8cb9216c9f3039e2647b990e97597cd1dfac044d
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-07 15:54:27 +0100

    feat: remove commented codes

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -37,28 +37,6 @@ const Hotel = () => {
     setSlideNumber(newSlideNumber);
   };
 
-  // const photos = [
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/hotel/square600/286659200.webp?k=9206fc9239b3e4538c22d04b85213d6d5e6257015022de8a37effd956fcde4b6&o=&s=1',
-  //   },
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/hotel/square600/280950287.webp?k=b984c542b8b1a2ee0e019e4491da338a85b660caa10c5e1197225c5f3052d629&o=&s=1',
-  //   },
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/hotel/square600/132452060.webp?k=8448bab55c87dbb42ab7c6643fbbce542124c908f63f6b36dc3cdc859e60cb0a&o=&s=1',
-  //   },
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/hotel/square600/187855604.webp?k=bbb45aa5b540c7608ea3af52d92b95a215df9af831dd3ae0e4c4cce501e28b1b&o=&s=1',
-  //   },
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/city/square250/822312.webp?k=f0e5aa24884bf61ddc08284c59807fa7d3a66b72fbdcec15488faf45824143b6&o=',
-  //   },
-  //   {
-  //     src: 'https://t-cf.bstatic.com/xdata/images/city/square250/684500.webp?k=df54bcea224564a0a00497a2076d5338316a0b56692498eddb3c02c9a6cdde64&o=',
-  //   },
-  // ];
-
-
   return (
     <div>
       <Header type='list' />
