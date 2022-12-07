81dca0339119ee780989a2021aa4c9708e7327e6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 00:39:11 +0100

    feat: add login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Home from './home/Home';
 import Hotel from './hotel/Hotel';
 import Layout from './layout/Layout';
 import List from './list/List';
+import Login from './login/Login';
 import NotFound from './notFound/NotFound';
 
-export { Home, Hotel, Layout, List, NotFound };
+export { Home, Hotel, Layout, List, Login, NotFound };
