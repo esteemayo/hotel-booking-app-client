99cd6a469140cc8367370e11da4490b89c6284f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 00:30:20 +0100

    feat: create AuthReducer

---
+++ src/context/auth/AuthReducer.js
@@ -0,0 +1,43 @@
+import { setToStorage, tokenKey } from 'utils';
+import { LOGIN_FAILURE, LOGIN_START, LOGIN_SUCCESS, LOGOUT } from './AuthTypes';
+
+const AuthReducer = (state, { type, payload }) => {
+  switch (type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        user: null,
+        loading: true,
+        error: null,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: setToStorage(tokenKey, payload),
+        error: null,
+      };
+
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        user: null,
+        loading: false,
+        error: payload,
+      };
+
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+        loading: false,
+        error: null,
+      };
+
+    default:
+      return state;
+  };
+};
+
+export default AuthReducer;
