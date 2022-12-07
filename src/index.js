2c15605e1ed1dd7e0f90d4bfe07b1fa60d76de6c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-07 16:15:56 +0100

    feat: add SearchProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SearchProvider } from 'context/search/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
   </React.StrictMode>
 );
 
