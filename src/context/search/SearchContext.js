b835eaae435357e0fe0752296aa63b8ab7fe73bb
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-07 16:13:55 +0100

    feat: create search context

---
+++ src/context/search/SearchContext.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useReducer } from 'react';
+
+import SearchReducer from './SearchReducer';
+import { NEW_SEARCH, RESET_SEARCH } from './SearchTypes';
+
+const INITIAL_STATE = {
+  city: undefined,
+  dates: [],
+  options: {
+    adult: undefined,
+    children: undefined,
+    room: undefined,
+  },
+};
+
+const SearchContext = createContext(INITIAL_STATE);
+
+const SearchProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE);
+
+  const newSearch = (search) => {
+    dispatch({
+      type: NEW_SEARCH,
+      payload: search,
+    });
+  };
+
+  const resetSearch = () => {
+    dispatch({
+      type: RESET_SEARCH,
+    })
+  }
+
+  return (
+    <SearchContext.Provider value={{ ...state, newSearch, resetSearch }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearchGlobalContext = () => {
+  return useContext(SearchContext);
+};
+
+export { SearchProvider };
