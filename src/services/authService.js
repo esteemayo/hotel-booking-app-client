26b9d9890ee198f4f13378379cd09f0b7d810751
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 00:30:50 +0100

    feat: add auth api endpoints

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,1 +1,8 @@
 import http from './httpService';
+import { getFromStorage, tokenKey } from 'utils';
+
+const apiEndpoint = '/auth';
+
+export const login = (credentials) => http.post(`${apiEndpoint}/login`, credentials);
+
+export const getJWT = () => getFromStorage(tokenKey)?.token;
