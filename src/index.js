1ca25be8cb3a021fef42d354dfabceb051bf9e8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 00:31:19 +0100

    feat: add AuthProvider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from 'context/auth/AuthContext';
 import { SearchProvider } from 'context/search/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchProvider>
-      <App />
-    </SearchProvider>
+    <AuthProvider>
+      <SearchProvider>
+        <App />
+      </SearchProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
