c9cdacbb08d945c4ca67fdb66c93e3f94566c1d1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 00:31:41 +0100

    feat: add helpers function

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+export const tokenKey = 'jwt_token';
+
+export const getFromStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
+
+export const setToStorage = (key, value) => {
+  return localStorage.setItem(key, JSON.stringify(value));
+};
