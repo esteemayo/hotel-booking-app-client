2a22f1f0e3fa842cd85a8e22aedab390eca590f6
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 13:12:40 +0100

    feat: import useNavigate

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useGlobalAuthContext } from 'context/auth/AuthContext';
 import './navbar.scss';
