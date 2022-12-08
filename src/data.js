27cdc6609661969550d8d0464085a6f2c13f3f59
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 13:29:27 +0100

    feat: add register input fields data

--- src/data.js
+++ src/data.js
@@ -10,3 +10,31 @@ export const loginInputs = [
     placeholder: 'password',
   },
 ];
+
+export const registerInputs = [
+  {
+    type: 'text',
+    id: 'name',
+    placeholder: 'name',
+  },
+  {
+    type: 'text',
+    id: 'username',
+    placeholder: 'username',
+  },
+  {
+    type: 'email',
+    id: 'email',
+    placeholder: 'email',
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'password',
+  },
+  {
+    type: 'password',
+    id: 'confirmPassword',
+    placeholder: 'confirm password',
+  },
+];
