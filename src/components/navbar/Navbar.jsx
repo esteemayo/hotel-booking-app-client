18d4c1567fc636a6d113aaa7a11c784dd6da1a09
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 04:11:39 +0100

    feat: implement user authorization

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
+
+import { useGlobalAuthContext } from 'context/auth/AuthContext';
 import './navbar.scss';
 
 const Navbar = () => {
+  const { user } = useGlobalAuthContext();
+
   return (
     <div className='navbar'>
       <div className='navbar__container'>
@@ -9,8 +13,17 @@ const Navbar = () => {
           <Link to='/'>Bookingapp</Link>
         </span>
         <div className='navbar__items'>
-          <button className='navbar__items--button'>Register</button>
-          <button className='navbar__items--button'>Login</button>
+          {user ? (
+            <>
+              <span>{user?.username}</span>
+              <button className='navbar__items--button'>Logout</button>
+            </>
+          ) : (
+            <>
+              <button className='navbar__items--button'>Register</button>
+              <button className='navbar__items--button'>Login</button>
+            </>
+          )}
         </div>
       </div>
     </div>
