b2abb959583aff554cf9285326471be691c825ad
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 04:40:55 +0100

    feat: add ProtectedRoute to login route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { loginInputs } from 'data';
 import { Home, Hotel, Layout, List, Login, NotFound } from 'pages';
+import ProtectedRoute from 'utils/ProtectedRoute';
 
 function App() {
   return (
@@ -11,7 +12,13 @@ function App() {
           <Route index element={<Home />} />
           <Route path='hotels' element={<List />} />
           <Route path='hotels/:slug' element={<Hotel />} />
-          <Route path='/login' element={<Login inputs={loginInputs} />} />
+          <Route path='/login'
+            element={
+              <ProtectedRoute>
+                <Login inputs={loginInputs} />
+              </ProtectedRoute>
+            }
+          />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
