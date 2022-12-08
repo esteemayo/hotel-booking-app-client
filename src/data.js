17e349ee11d702d34338bd58e49f2a340bc5d2d3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-08 03:55:35 +0100

    feat: login inputs data

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const loginInputs = [
+  {
+    type: 'text',
+    id: 'username',
+    placeholder: 'username',
+  },
+  {
+    type: 'password',
+    id: 'password',
+    placeholder: 'password',
+  },
+];
