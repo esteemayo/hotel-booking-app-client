f33cd1b81bf6f9f75b2d67560650c162e785f27d
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-09 14:03:37 +0100

    feat: import usenavigate hook

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
