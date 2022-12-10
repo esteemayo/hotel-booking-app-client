af330f53cf086b3ba1f499379f683176ca90faff
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-10 01:58:51 +0100

    feat: add hotel api call endpoint

--- src/services/hotelService.js
+++ src/services/hotelService.js
@@ -1,1 +1,6 @@
 import http from './httpService';
+
+const apiEndpoint = '/hotels';
+
+export const getHotelCountByCity = () =>
+  http.get(`${apiEndpoint}/count-by-city?cities=madrid,barcelona,london`);
