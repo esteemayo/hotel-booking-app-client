6962c82a4afeb026b63189cfd98f5583fbe6775f
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-12 18:11:28 +0100

    modified

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -32,7 +32,7 @@ const Hotel = () => {
     return diffDays;
   };
 
-  const days = dayDifference(dates[0][0].endDate, dates[0][0].startDate);
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   const handleOpen = (index) => {
     setSlideNumber(index);
