0f48ecffe2255d9ee6be731464768ff5e54434a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-21 02:48:09 +0100

    feat: add a new action type

--- src/context/auth/AuthTypes.js
+++ src/context/auth/AuthTypes.js
@@ -2,3 +2,4 @@ export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT = 'LOGOUT';
+export const RESET = 'RESET';
